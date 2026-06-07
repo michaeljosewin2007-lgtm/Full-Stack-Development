7af921958c2a7471dd53abd1ca5c186d2c9a733a
michaeljosewin2007-lgtm <michaeljosewin2007@gmail.com>
2026-06-07 20:56:28 +0530

    Add files via upload

---
+++ day-18/TEACHER/public/script.js
@@ -0,0 +1,349 @@
+// Priority Glow Logic
+console.log("SCRIPT LOADED");
+ 
+const taskCards =
+document.querySelectorAll(".task-card");
+
+taskCards.forEach(function(card) {
+
+    const priorityText =
+    card.querySelector(".priority").innerText;
+
+    if (priorityText.includes("High")) {
+
+        card.style.boxShadow =
+        "0 0 20px rgba(255, 0, 0, 0.5)";
+    }
+ 
+    else if (priorityText.includes("Medium")) {
+
+        card.style.boxShadow =
+        "0 0 20px rgba(255, 255, 0, 0.4)";
+    }
+
+    else {
+
+        card.style.boxShadow =
+        "0 0 20px rgba(0, 255, 150, 0.4)";
+    }
+});
+// ======================
+// GET TASKS
+// ======================
+
+document
+.getElementById("getTasksBtn")
+.addEventListener("click", async function(){
+
+    const response =
+    await fetch(
+    "http://localhost:3000/teacher/studentTasks"
+);
+
+    const tasks =
+    await response.json();
+
+    let output = "";
+
+    tasks.forEach(function(task){
+
+    output += `
+    <div class="student-card">
+
+        <h3>${task.title}</h3>
+
+        <p>📚 ${task.subject}</p>
+
+        <p>⚡ ${task.priority}</p>
+
+        ${
+            task.teacher_id !== null
+            ?
+            `<button
+                class="delete-task-btn"
+                data-teacherid="${task.teacher_id}"
+            >
+                Delete ❌
+            </button>`
+            :
+            ""
+        }
+
+    </div>
+    `;
+});
+
+    document
+    .getElementById(
+        "studentTasksContainer"
+    )
+    .innerHTML = output;
+    document
+.querySelectorAll(".delete-task-btn")
+.forEach(function(button){
+
+    button.addEventListener(
+        "click",
+        async function(){
+
+            const teacherId =
+button.dataset.teacherid;
+
+await fetch(
+    `http://localhost:4000/teacherTasks/${teacherId}`,
+    {
+        method: "DELETE"
+    }
+);
+
+            button
+            .closest(".student-card")
+            .remove();
+
+            document
+            .getElementById("apiResult")
+            .innerHTML =
+            "✅ Task deleted";
+        }
+    );
+});
+
+    document
+    .getElementById("apiResult")
+    .innerHTML =
+    `✅ ${tasks.length} tasks fetched successfully`;
+});
+
+
+// ======================
+// POST TASK
+// ======================
+
+document
+.getElementById("postTaskBtn")
+.addEventListener("click", async function(){
+
+    const title =
+    document.querySelector(
+        'input[name="title"]'
+    ).value;
+
+    const subject =
+    document.querySelector(
+        'input[name="subject"]'
+    ).value;
+
+    const priority =
+    document.querySelector(
+        'select[name="priority"]'
+    ).value;
+
+    const deadline =
+    document.querySelector(
+        'input[name="deadline"]'
+    ).value;
+
+    console.log(title);
+    console.log(subject);
+    console.log(priority);
+    console.log(deadline);
+
+    await fetch(
+    "http://localhost:4000/teacherTasks",
+        {
+            method: "POST",
+
+            headers: {
+                "Content-Type":
+                "application/json"
+            },
+
+            body: JSON.stringify({
+
+                title,
+                subject,
+                deadline,
+                priority
+            })
+        }
+    );
+
+    document
+    .getElementById("apiResult")
+    .innerHTML =
+    "✅ Teacher task sent successfully";
+});
+
+
+// Smart Suggestion System
+
+const suggestionText =
+document.getElementById("suggestionText");
+
+
+
+const allCards =
+document.querySelectorAll(".task-card");
+
+
+
+let mostUrgentTask = null;
+
+let smallestDays = Infinity;
+
+
+
+allCards.forEach(function(card){
+
+    const deadlineText =
+    card.querySelector(".deadline").innerText;
+
+    const cleanDate =
+    deadlineText.replace("Deadline: ", "");
+
+    const taskDate =
+    new Date(cleanDate);
+
+    const today =
+    new Date();
+
+    const difference =
+    taskDate - today;
+
+    const daysLeft =
+    Math.ceil(
+        difference /
+        (1000 * 60 * 60 * 24)
+    );
+
+
+
+    if (daysLeft < smallestDays) {
+
+        smallestDays = daysLeft;
+
+        mostUrgentTask =
+        card.querySelector("h3").innerText;
+    }
+});
+
+
+
+if (mostUrgentTask) {
+
+    suggestionText.innerText =
+    `⚡ Focus on "${mostUrgentTask}" — deadline approaching!`;
+}
+
+const allPriorityText =
+document.querySelectorAll(".priority");
+
+
+
+allPriorityText.forEach(function(priority){
+
+    const text =
+    priority.innerText;
+
+
+
+    if (text.includes("High")) {
+
+        priority.style.color =
+        "#ef4444";
+
+        priority.style.textShadow =
+        "0 0 10px rgba(239,68,68,0.7)";
+        priority.closest(".task-card").style.border =
+"1px solid rgba(239,68,68,0.5)";
+    }
+
+
+
+    else if (text.includes("Medium")) {
+
+        priority.style.color =
+        "#facc15";
+
+        priority.style.textShadow =
+        "0 0 10px rgba(250,204,21,0.7)";
+        priority.closest(".task-card").style.border =
+"1px solid rgba(250,204,21,0.5)";
+    }
+
+
+
+    else {
+
+        priority.style.color =
+        "#38bdf8";
+
+        priority.style.textShadow =
+        "0 0 10px rgba(56,189,248,0.7)";
+        priority.closest(".task-card").style.border =
+"1px solid rgba(56,189,248,0.5)";
+    }
+});
+
+const modeToggle =
+document.getElementById("modeToggle");
+
+
+
+modeToggle.addEventListener("change", function(){
+
+    if (modeToggle.checked) {
+
+        // Focus Mode
+        localStorage.setItem(
+            "studyflowMode",
+            "focus"
+        );
+
+        document.body.style.background =
+        "linear-gradient(-45deg, #111827, #1e1b4b, #312e81, #111827)";
+
+        document.body.style.backgroundSize =
+        "400% 400%";
+
+        document.body.style.animation =
+        "gradientMove 10s ease infinite";
+    }
+
+    else {
+
+        // Chill Mode
+        localStorage.setItem(
+    "studyflowMode",
+    "chill"
+);
+
+        document.body.style.background =
+        "linear-gradient(-45deg, #0f172a, #164e63, #0f766e, #0f172a)";
+
+        document.body.style.backgroundSize =
+        "400% 400%";
+
+        document.body.style.animation =
+        "gradientMove 10s ease infinite";
+    }
+});
+const savedMode =
+localStorage.getItem("studyflowMode");
+
+if (savedMode === "focus") {
+
+    modeToggle.checked = true;
+
+    modeToggle.dispatchEvent(
+        new Event("change")
+    );
+}
+else if (savedMode === "chill") {
+
+    modeToggle.checked = false;
+
+    modeToggle.dispatchEvent(
+        new Event("change")
+    );
+}
---
+++ day-18/app.js
@@ -0,0 +1,355 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+import db from "./db.js";
+import pool from "./db.js";
+import bcrypt from "bcrypt";
+import session from "express-session";
+
+const app = express();
+
+
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({
+
+        secret:
+        "studyflow-secret-key",
+
+        resave: false,
+
+        saveUninitialized: false
+    })
+);
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+db.query("SELECT NOW()")
+.then(result => { 
+
+    console.log(
+        "Database Connected:",
+        result.rows[0]
+    );
+
+})
+.catch(error => {
+
+    console.log(error);
+});
+ 
+app.get("/auth", function(req,res){
+
+    res.render("login-register", {
+
+        registerError: null,
+        loginError: null
+    });
+});
+app.post("/register", async function(req,res){
+
+    const { name, email, password } = req.body;
+
+    const existingUser =
+    await pool.query(
+
+        `SELECT *
+         FROM users
+         WHERE email = $1`,
+
+        [email]
+    );
+
+    if(existingUser.rows.length > 0){
+
+        return res.render("login-register", {
+
+    registerError: "Email already registered",
+    loginError: null
+});
+    }
+
+    const hashedPassword =
+    await bcrypt.hash(password, 10);
+
+    await pool.query(
+
+        `INSERT INTO users
+        (name,email,password)
+
+        VALUES($1,$2,$3)`,
+
+        [
+            name,
+            email,
+            hashedPassword
+        ]
+    );
+
+    res.redirect("/auth");
+});
+app.post("/login", async function(req,res){
+
+    const { email, password } = req.body;
+
+    const result =
+    await pool.query(
+
+        `SELECT *
+         FROM users
+         WHERE email = $1`,
+
+        [email]
+    );
+
+    if(result.rows.length === 0){
+
+        return res.render("login-register", {
+
+    registerError: null,
+    loginError: "User not found. Please register first."
+});
+    }
+
+    const user =
+    result.rows[0];
+
+    const match =
+    await bcrypt.compare(
+        password,
+        user.password
+    );
+
+    if(!match){
+
+        return res.render("login-register", {
+
+    registerError: null,
+    loginError: "Email or password incorrect"
+});
+    }
+    req.session.userId =
+user.id;
+
+req.session.userName =
+user.name;
+
+    res.redirect("/");
+});
+app.get("/logout", function(req,res){
+
+    req.session.destroy(function(){
+
+        res.redirect("/auth");
+    });
+});
+
+
+app.get("/", async function (req, res) {
+    if(!req.session.userId){
+
+    return res.redirect("/auth"); 
+}
+
+    const result =
+await pool.query(
+
+    `SELECT *
+     FROM tasks
+     WHERE user_id = $1
+     ORDER BY id DESC`,
+
+    [req.session.userId]
+); 
+
+    res.render("index", {
+
+        tasks: result.rows
+    });
+});
+
+app.post("/addTask", async function(req,res){
+
+    await pool.query(
+
+    `INSERT INTO tasks
+    (
+        user_id,
+        title,
+        subject,
+        deadline,
+        priority
+    )
+
+    VALUES ($1,$2,$3,$4,$5)`,
+
+        [
+    req.session.userId,
+    req.body.title,
+    req.body.subject,
+    req.body.deadline,
+    req.body.priority
+]
+    );
+
+    res.redirect("/");
+});
+app.post(
+"/completeTask/:id",
+
+async function(req,res){
+
+    const taskId =
+    Number(req.params.id);
+
+    await pool.query(
+
+        `DELETE FROM tasks
+         WHERE id = $1`,
+
+        [taskId]
+    );
+
+    res.redirect("/");
+});
+app.get("/tasks", async (req, res) => {
+
+    const result =
+    await pool.query(
+    `SELECT *
+     FROM tasks
+     WHERE user_id = $1
+     ORDER BY id DESC`,
+
+    [req.session.userId]
+);
+
+    res.json(result.rows);
+
+});
+app.get(
+"/teacher/studentTasks",
+
+async function(req,res){
+
+    const result =
+    await pool.query(
+
+        `SELECT *
+         FROM tasks
+         ORDER BY id DESC`
+    );
+
+    res.json(result.rows);
+});
+app.post("/importTeacherTasks", async (req, res) => {
+
+    const exists =
+    await pool.query(
+
+    `INSERT INTO tasks
+    (
+        user_id,
+        teacher_id,
+        title,
+        subject,
+        deadline,
+        priority
+    )
+
+    VALUES ($1,$2,$3,$4,$5,$6)`,
+
+    [
+        req.session.userId,
+        req.body.teacherId,
+        req.body.title,
+        req.body.subject,
+        req.body.deadline,
+        req.body.priority
+    ]
+);
+
+    if (exists.rows.length > 0) {
+
+        return res.json({
+            message: "Task already imported"
+        });
+    }
+
+    await pool.query(
+
+        `INSERT INTO tasks
+        (
+            teacher_id,
+            title,
+            subject,
+            deadline,
+            priority
+        )
+
+        VALUES ($1,$2,$3,$4,$5)`,
+
+        [
+            req.body.teacherId,
+            req.body.title,
+            req.body.subject,
+            req.body.deadline,
+            req.body.priority
+        ]
+    );
+
+    res.json({
+        message: "Imported"
+    });
+});
+app.delete("/tasks/:id", async (req,res)=>{
+
+    const taskId =
+    Number(req.params.id);
+
+    await pool.query(
+
+        `DELETE FROM tasks
+         WHERE id = $1`,
+
+        [taskId]
+    );
+
+    res.json({
+        message:
+        "Task deleted"
+    });
+});
+app.delete(
+"/teacherImportedTasks/:teacherId",
+
+async (req,res)=>{
+
+    const teacherId =
+    Number(req.params.teacherId);
+
+    await pool.query(
+
+        `DELETE FROM tasks
+         WHERE teacher_id = $1`,
+
+        [teacherId]
+    );
+
+    res.json({
+        message:"Imported tasks removed"
+    });
+});
+
+
+
+app.listen(3000, function () {
+
+    console.log("Server running on port 3000");
+});
